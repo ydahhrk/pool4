3148977e7f32a92f828f17f71666664a8ae0cfe7
frutos93 <cefg93@gmail.com>
2015-12-08 16:19:10 -0600

    Client
    Client functions

---
+++ src/client/prefixes.h
@@ -0,0 +1,40 @@
+/*
+ * prefixes.h
+ *
+ *  Created on: Nov 19, 2015
+ *      Author: cfrutos
+ */
+
+#include "types.h"
+#ifndef PREFIXES_H_
+#define PREFIXES_H_
+
+static inline bool ipv6_addr_equal(const struct in6_addr *a1, const struct in6_addr *a2)
+{
+	#if defined(CONFIG_HAVE_EFFICIENT_UNALIGNED_ACCESS) && BITS_PER_LONG == 64
+	const unsigned long *ul1 = (const unsigned long *)a1;
+	const unsigned long *ul2 = (const unsigned long *)a2;
+
+	return ((ul1[0] ^ ul2[0]) | (ul1[1] ^ ul2[1])) == 0UL;
+	#else
+	return ((a1->s6_addr32[0] ^ a2->s6_addr32[0]) |
+			(a1->s6_addr32[1] ^ a2->s6_addr32[1]) |
+			(a1->s6_addr32[2] ^ a2->s6_addr32[2]) |
+			(a1->s6_addr32[3] ^ a2->s6_addr32[3])) == 0;
+ #endif
+}
+bool prefix6_equals(const struct ipv6_prefix *expected, const struct ipv6_prefix *actual)
+{
+	if (expected == actual)
+		return true;
+	if (expected == NULL || actual == NULL)
+		return false;
+	if (!ipv6_addr_equal(&expected->address, &actual->address))
+		return false;
+	if (expected->len != actual->len)
+		return false;
+
+	return true;
+}
+
+#endif /* PREFIXES_H_ */
---
+++ src/client/types.h
@@ -0,0 +1,47 @@
+#ifndef _LINUX_TYPES_H
+#define _LINUX_TYPES_H
+
+#include <stdint.h>
+
+typedef uint8_t __u8;
+typedef uint16_t __u16;
+typedef uint32_t __u32;
+typedef uint64_t __u64;
+
+typedef int bool;
+
+#ifndef false
+#define false 0
+#endif
+
+#ifndef true
+#define true 1
+#endif
+
+#define INET_ADDRSTRLEN		(16)
+#define INET6_ADDRSTRLEN	(48)
+#define AF_INET6		(10)
+
+
+struct in6_addr
+{
+	union
+	{
+		__u8 u6_addr8[16];
+		__u16 u6_addr16[8];
+		__u32 u6_addr32[4];
+	} in6_u;
+
+#define s6_addr in6_u.u6_addr8
+#define s6_addr16 in6_u.u6_addr16
+#define s6_addr32 in6_u.u6_addr32
+};
+
+struct ipv6_prefix {
+	/** IPv6 prefix. */
+	struct in6_addr address;
+	/** Number of bits from "address" which represent the network. */
+	__u8 len;
+};
+
+#endif
