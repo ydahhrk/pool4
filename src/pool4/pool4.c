6dee4b8008decffeef21864cbf9d4de9e6913a7b
ddnov89 <ing.alexavalos@gmail.com>
2015-12-18 16:01:09 -0600

    Update pool4.c
    First revision corrected.

--- src/pool4/pool4.c
+++ src/pool4/pool4.c
@@ -2,21 +2,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "types.h"
-#include "in.h"
-#include "port_range.h"
 #include "list.h"
-
-#define cpu_to_be32(x) x
-
-typedef enum { false, true } bool;
-
-struct pool4_entry {
-	__u32 mark;
-	__u8 proto;
-	struct in_addr addr;
-	struct port_range range;
-	struct list_hook list;
-} pool4_dbase;
+#include "pool4.h"
 
 struct pool4_entry pool4_list;
 
@@ -25,23 +12,31 @@ void pool4_init()
 	INIT_LIST_HEAD(&pool4_list.list);
 }
 
-void pool4_add(__u32 mark, __u8 proto,  struct in_addr addr,
+int pool4_add(__u32 mark, __u8 proto,  struct in_addr addr,
 		struct port_range *range, struct pool4_entry *entry)
 {
-	printf("pool4_add() started running...\n");
+	int err;
 	struct pool4_entry *tmp;
 	tmp = malloc(sizeof(struct pool4_entry));
+	if (!tmp) {
+		err = -ENOMEM;
+	} else {
+		err = 0;
+	}
+
 	tmp->mark = mark;
 	tmp->proto = proto;
 	tmp->addr.s_addr = addr.s_addr;
 	tmp->range.min = range->min;
 	tmp->range.max = range->max;
-	list_add(&(tmp->list), &(entry->list));
+	list_add_tail(&(tmp->list), &(entry->list));
+	return err;
 }
 
-bool pool4_entry_equals(struct pool4_entry new, struct pool4_entry old)
+static bool pool4_entry_equals(struct pool4_entry new, struct pool4_entry old)
 {
 	bool equals;
+
 	if (new.mark == old.mark && new.proto == old.proto &&
 			new.addr.s_addr == old.addr.s_addr &&
 			new.range.max == old.range.max &&
@@ -53,62 +48,122 @@ bool pool4_entry_equals(struct pool4_entry new, struct pool4_entry old)
 	return equals;
 }
 
-void pool4_rm(__u32 mark, __u8 proto, struct in_addr addr,
-		struct port_range *range, struct list_hook *entry)
+int pool4_rm(__u32 mark, __u8 proto, struct in_addr addr,
+		struct port_range *range, struct pool4_entry *entry)
 {
 	struct list_hook *iter;
+	struct list_hook *tmp;
 	struct pool4_entry *tmp1;
 	struct pool4_entry *tmp2;
+	int err;
+
 	tmp1 = malloc(sizeof(struct pool4_entry));
+	if (!tmp1) {
+		err = -ENOMEM;
+	} else {
+		err = 0;
+	}
+
 	tmp1->mark = mark;
 	tmp1->proto = proto;
 	tmp1->addr.s_addr = addr.s_addr;
 	tmp1->range.min = range->min;
 	tmp1->range.max = range->max;
 
-	list_for_each(iter, entry) {
+	list_for_each_safe(iter, tmp, &entry->list) {
 		tmp2 = list_entry(iter, struct pool4_entry, list);
-		if(pool4_entry_equals(*tmp1, *tmp2)) {
+		if (pool4_entry_equals(*tmp1, *tmp2)) {
 			list_del(&tmp2->list);
 			free(tmp1);
 			free(tmp2);
-			return;
 		}
 	}
+	return err;
 }
 
-void pool4_flush(struct list_hook *head)
+int pool4_flush(struct pool4_entry *entry)
 {
+	printf("List flushed.\n\n");
+	int err;
 	struct list_hook *iter;
 	struct list_hook *tmp;
-	struct pool4_entry *ptr;
+	struct pool4_entry *tmpx;
 
-	list_for_each_safe(iter, tmp, head) {
-		ptr = list_entry(iter, struct pool4_entry, list);
-		list_del(&ptr->list);
-		free(ptr);
+	tmpx = malloc(sizeof(struct pool4_entry));
+		if (!tmpx) {
+			err = -ENOMEM;
+		} else {
+			err = 0;
+		}
+
+	list_for_each_safe(iter, tmp, &entry->list) {
+		tmpx = list_entry(iter, struct pool4_entry, list);
+		list_del(iter);
+		free(tmpx);
 	}
+	return err;
 }
 
-bool pool4_is_empty(struct pool4_entry entry)
+bool pool4_is_empty(struct pool4_entry *entry)
 {
 	bool empty;
-	if(!list_empty(&entry.list)){
+
+	if (!list_empty(&entry->list)){
 		empty = false;
+		printf("It is not empty.\n\n");
 	} else {
 		empty = true;
+		printf("It is empty.\n\n");
 	}
 	return empty;
 }
 
-void pool4_print_all(struct pool4_entry entry)
+void pool4_print_all(struct pool4_entry *entry)
 {
-	struct pool4_entry *iter;
+	printf("Elements in the list:\n\n");
+	struct list_hook *iter;
+	struct list_hook *tmp;
+	struct pool4_entry *tmpx;
 
-	list_for_each_entry(iter, &entry.list, list) {
-		printf("%u, ", iter->mark);
-		printf("%u, ", iter->proto);
-		printf("%pI4, ", &iter->addr.s_addr);
-		printf("%u-%u\n", iter->range.min, iter->range.max);
+	list_for_each_safe(iter, tmp, &entry->list) {
+		tmpx = list_entry(iter, struct pool4_entry, list);
+		printf("%u, ", tmpx->mark);
+		printf("%u, ", tmpx->proto);
+		printf("%pI4, ", &tmpx->addr.s_addr);
+		printf("%u-%u\n", tmpx->range.min, tmpx->range.max);
 	}
 }
+
+//In construction...
+/*
+bool pool4_contains(struct pool4_entry new, struct pool4_entry *entry)
+{
+	bool found = false;
+	int err;
+	struct list_hook *iter;
+	struct list_hook *tmp;
+	struct pool4_entry *tmp1;
+	struct pool4_entry *tmp2;
+
+	tmp1 = malloc(sizeof(struct pool4_entry));
+	if (!tmp1){
+		err = -ENOMEM;
+	} else {
+		err = 0;
+	}
+
+	tmp1->mark = new.mark;
+	tmp1->proto = new.proto;
+	tmp1->addr.s_addr = new.addr.s_addr;
+	tmp1->range.min = new.range.min;
+	tmp1->range.max = new.range.max;
+
+	list_for_each_safe(iter, tmp, &entry->list){
+		tmp2 = list_entry(iter, struct pool4_entry, list);
+		if (pool4_entry_equals(*tmp1, *tmp2)){
+			found = true;
+		}
+	}
+	return found;
+}
+*/
