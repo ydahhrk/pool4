d57c036870a4e2875bbdd6558d8aa423e60dc307
ddnov89 <ing.alexavalos@gmail.com>
2015-12-18 15:56:54 -0600

    Update main.c
    Adding some functions tests to main.

--- src/pool4/main.c
+++ src/pool4/main.c
@@ -1,10 +1,11 @@
-#include "pool4.c"
+#include "types.h"
+#include "list.h"
+#include "pool4.h"
 
 int main()
 {
 	struct pool4_entry test_list;
 	INIT_LIST_HEAD(&test_list.list);
-
 
 	struct pool4_entry *one = malloc(sizeof(*one));
 	one->mark = 1;
@@ -27,14 +28,50 @@ int main()
 	three->range.min = 3;
 	three->range.max = 3;
 
-	pool4_add(one->mark, one->proto, one->addr, &one->range,
+	//Adding 3 elements to the list: test_list
+	printf("Adding 3 elements to the list...\n\n");
+	pool4_add(one->mark, one->proto, one->addr, &one->range, &test_list);
+
+	pool4_add(two->mark, two->proto, two->addr, &two->range, &test_list);
+
+	pool4_add(three->mark, three->proto, three->addr, &three->range,
 			&test_list);
 
-	pool4_add(two->mark, two->proto, two->addr, &two->range,
-			&test_list);
+	//Checking if list is empty...
+	printf("Checking if the list is empty:\n");
+	pool4_is_empty(&test_list);
 
-	pool4_add(three->mark, three->proto, three->addr,
-			&three->range, &test_list);
+	//Printing the whole list...
+	printf("Printing the list...\n");
+	pool4_print_all(&test_list);
+	printf("\n");
+
+	//Removing structure 'one' and printing...
+	printf("Removing structure 'one' and printing list...\n");
+	pool4_rm(one->mark, one->proto, one->addr, &one->range, &test_list);
+	pool4_print_all(&test_list);
+	printf("\n");
+
+	//Removing structure 'two' and printing...
+	printf("Removing structure 'two' and printing list...\n");
+	pool4_rm(two->mark, two->proto, two->addr, &two->range, &test_list);
+	pool4_print_all(&test_list);
+	printf("\n");
+
+	//Adding again removed elements and printing...
+	printf("Adding again removed elements and printing...\n");
+	pool4_add(one->mark, one->proto, one->addr, &one->range, &test_list);
+	pool4_add(two->mark, two->proto, two->addr, &two->range, &test_list);
+	pool4_print_all(&test_list);
+	printf("\n");
+
+	//Flushing the list...
+	printf("Flushing the list...\n");
+	pool4_flush(&test_list);
+	pool4_print_all(&test_list);
+
+	//Checking if list is empty...
+	pool4_is_empty(&test_list);
 
 	return 0;
 }
