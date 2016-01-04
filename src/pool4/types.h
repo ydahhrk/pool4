91507eae28ea0323a720ed155f19b4f5811742bf
frutos93 <cefg93@gmail.com>
2016-01-04 14:40:27 -0600

    domain mask and ipv4 transport in types.h
    Both structures were added to implement get_nth_ladder

--- src/pool4/types.h
+++ src/pool4/types.h
@@ -26,6 +26,7 @@ struct pool4_entry {
 	struct port_range range;
 	struct list_hook list;
 };
+
 struct ipv4_transport_addr {
 	struct in_addr l3;
 
@@ -35,11 +36,9 @@ struct ipv4_transport_addr {
 struct client_mask_domain {
 
 		struct ipv4_transport_addr first;
-
 		unsigned int step;
-
 		unsigned int count;
-	};
+};
 
 #define cpu_to_be32(x) x
 
